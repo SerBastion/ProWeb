45b1d0b3a0bd1c080157b2e756b40cbf6236366a
SerBastion <sebastian.garcianeives@gmail.com>
2023-11-14 07:57:46 -0600

    Commit Server

--- Parcial3/Server/Server.js
+++ Parcial3/Server/Server.js
@@ -1,6 +1,7 @@
 const http = require ('hhtp');
 
 const servidor = http.createServer((req,res)=>{
+    res.setHeader("Access")
     res.wite("Servidor http Node contestado a peditoin get")
     res.end();
 });
