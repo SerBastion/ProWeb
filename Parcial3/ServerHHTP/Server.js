6010864349af3f3dd9a954a395694b3dda2bcc23
SerBastion <sebastian.garcianeives@gmail.com>
2023-11-15 08:12:58 -0600

    Commit Cors

---
+++ Parcial3/ServerHHTP/Server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+ 
+const servidor = http.createServer((req, res) => {
+    //Configuración de encabezados para permitir CORS
+   
+    res.setHeader('Access-Control-Allow-Origin', '*');
+   
+    // Respuesta del servidorr
+    res.write("Servidor http Node contestado a petición GET");
+    res.end();
+});
+ 
+servidor.listen(8082, () => {
+    console.log("Servidor Node HTTP corriendo en el puerto 8082");
+});
+ 
